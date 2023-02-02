f151708e84e8e2c5992f303b8a58fffddaeff35e
Scorpionzaa555 <74282946+Scorpionzaa555@users.noreply.github.com>
2023-02-02 20:37:24 +0700

    Update lab02.py

--- lab02.py
+++ lab02.py
@@ -1,21 +1,34 @@
+class DataNode:
+    def __init__(self, input_name):
+        self.name = input_name
+        self.next = None
+
 class SinglyLinkedList:
-    
     def __init__(self):
         self.count = 0
         self.head = None
 
     def traverse(self):
-        print("Traverse: ", end='')
-        current_node = self.head
-        if current_node == None:
+        if self.head == None:
+            #empty list
             print("This is an empty list.")
-        while current_node != None:
-            print(current_node.name, end="")
-            current_node = current_node.next
-            if current_node != None:
-                print(" --> ", end="") 
-            else:
-                print("")
+        else:
+            '''
+            pos = self.head
+            while pos != None:
+                print(pos.name, "->", end=' ')
+                pos = pos.next
+            print("")
+            '''
+            pos = self.head
+            while pos != None:
+                print(pos.name, end="")
+                pos = pos.next
+                if pos != None:
+                    print(" -> ", end="") 
+                else:
+                    print("")
+        return
 
     def insertFront(self, data):
         pNew = DataNode(data)
@@ -25,78 +38,61 @@ class SinglyLinkedList:
             pNew.next = self.head
             self.head = pNew
         self.count += 1
+        return
 
     def insertLast(self, data):
         pNew = DataNode(data)
-        current_node = self.head
-        if self.head == None:
-            self.head == pNew
+        if self.head == None: #empty
+            self.head = pNew
         else:
-            while current_node.next != None:
-                current_node = current_node.next
-            current_node.next = pNew
-        self.count += 1
+            pos = self.head
+            while pos.next != None:
+                pos = pos.next
+            pos.next = pNew
+        return
     
     def insertBefore(self, node, data):
         pNew = DataNode(data)
-        current_node = self.head
-        prev_node = self.head
-        count = 0
-        if self.head.name == node:
-            pNew.next = self.head
-            self.head = pNew
-            self.count += 1
+        if self.head == None:
+            print("Cannot insert", node, "does not exist.")
         else:
-            while current_node != None:
-                if current_node.name == node:
-                    break
-                current_node = current_node.next
-                count += 1
-            if current_node == None:
-                print("Cannot insert, <node> does not exist.")
-            else:
-                for _ in range(count-1):
-                    prev_node = prev_node.next
-                prev_node.next = pNew
-                pNew.next = current_node
-                self.count += 1
-    
+            pos = self.head
+            while pos.next != None:
+                if pos.next.name == node:
+                    pNew.next = pos.next
+                    pos.next = pNew
+                    return
+                pos = pos.next
+        return
+
     def delete(self, data):
-        current_node = self.head
-        prev_node = self.head
-        count = 0
-        if self.head.name == data:
-            self.head = current_node.next
-            del current_node
-
+        if self.head == None:
+            print("Cannot delete,", data, "dose not exist.")
         else:
-            while current_node != None:
-                if current_node.name == data:
+            pos = self.head
+            while pos.next != None:
+                if pos.next.name == data:
                     break
-                current_node = current_node.next
-                count += 1
-            if current_node == None:
-                print("Cannot delete, <data> does not exist.")
-            else:
-                for _ in range(count-1):
-                    prev_node = prev_node.next
-                prev_node.next = current_node.next
-                del current_node
-                self.count -= 1
-       
-class DataNode:
-    def __init__(self, person_name=""):
-        self.name = person_name
-        self.next = None
-
+                pos = pos.next
+            pos.next = pos.next.next
+        self.count -= 1
+        return
 
 mylist = SinglyLinkedList()
-pNew = DataNode("Rain")
+pNew = DataNode("John")
 mylist.head = pNew
-pNew = DataNode("Phu")
+print(mylist.head.name)
+pNew = DataNode("Tony")
 mylist.head.next = pNew
-mylist.insertFront("Pare")
-mylist.insertLast("Eik")
-mylist.insertBefore("Rain", "Mark")
-mylist.delete("Frank")
+print(mylist.head.next.name)
+pNew = DataNode("Bill")
+mylist.head.next.next = pNew
+mylist.traverse()
+mylist.insertFront("Kim")
+mylist.traverse()
+mylist.insertLast("Pao")
+mylist.traverse()
+mylist.delete("Pao")
+mylist.traverse()
+mylist.insertBefore("Tony", "Eik")
 mylist.traverse()
