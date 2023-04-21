454dbf0e1bae9a3781dd62175f1d31da2ec20e40
Thanawan Chaemsathian <74282946+Scorpionzaa555@users.noreply.github.com>
2023-04-21 14:28:15 +0700

    Update Lab09_2.py

--- Lab/Lab09_2.py
+++ Lab/Lab09_2.py
@@ -1,17 +1,62 @@
+def insertionSort(list, last):
+    current = 1
+    compare = 0
+    while (current <= last):
+        hold = list[current]
+        walker = current - 1
+        compare += 1
+        while (walker >= 0 and cards_list(list[walker]) > cards_list(hold)):
+            list[walker+1] = list[walker]
+            walker -= 1
+            compare += 1
+        list[walker+1] = hold
+        current += 1
+        if walker < 0:
+            compare -= 1
+        print(list)
+    print(compare)
+#####################
 def selectionSort(list, last):
     current = 0
     compare = 0
-    while(current < last):
+    while current < last:
         smallest = current
-        walker = current + 1
-        while(walker <= last):
-            compare += 1
-            if(list[walker] < list[smallest]):
+        walker = current+1
+        while walker <= last:
+            if cards_list(list[walker]) <= cards_list(list[smallest]):
                 smallest = walker
             walker += 1
+            compare += 1
         list[current], list[smallest] = list[smallest], list[current]
         current += 1
         print(list)
-    print("Comparison times:", compare)
+    print("Comparison times: " , compare)
+####################
+def bubbleSort(list, last):
+    current = 0
+    compare = 0
+    sort = False
+    while (current <= last) and (sort == False):
+        walker = last
+        sort = True
+        while walker > current:
+            if cards_list(list[walker]) < cards_list(list[walker-1]):
+                sort = False
+                list[walker], list[walker-1] = list[walker-1], list[walker]
+            walker -=1
+            compare += 1
+        current += 1
+        print(list)
+    print("Comparison times: " , compare)
+#####################
+def cards_list(list):
+    nums = {"2" :1, "3" :2, "4" :3, "5" :4, "6" :5, "7" :6, "8" :7, "9" :8,
+            "10" :9, "J" :10, "Q" :11, "K" :12, "A" :13}
+    signs = {"♣" :1, "♦" :2, "♥" :3, "♠" :4}
 
-selectionSort([23, 78, 45, 8, 32, 56, -1, 0, -256], 8)
+    lists = (nums[list[:-1]], signs[list[-1]])
+    return lists
+
+insertionSort(['4♣', 'A♣', '10♥', 'K♦', '4♠', '10♣', '3♦', '7♥', '4♦'], 8)
+selectionSort(['4♣', 'A♣', '10♥', 'K♦', '4♠', '10♣', '3♦', '7♥', '4♦'], 8)
+bubbleSort(['4♣', 'A♣', '10♥', 'K♦', '4♠', '10♣', '3♦', '7♥', '4♦'], 8)
